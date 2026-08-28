9569a1f4551ddd1b7bfebfd4af2b8de65a781727
Victor Diaz <vjdiazt@unimagdalena.edu.co>
2026-08-27 20:22:20 -0500

    creacion de specs

---
+++ src/main/java/com/hospitua/HospituaApplication.java
@@ -0,0 +1,13 @@
+package com.hospitua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospituaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospituaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitua/HospituaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospituaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
